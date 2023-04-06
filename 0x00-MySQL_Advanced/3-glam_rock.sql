78fda61544b87c4116a48f00651a58630be1b153
iamqaasim <iamqaasim@gmail.com>
2023-04-06 04:43:59 -0700

    My first commit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,5 @@
-SELECT 
-    band_name, 
-    YEAR(MAX(split)) - YEAR(MIN(formed)) AS lifespan 
-FROM 
-    metal_bands 
-WHERE 
-    style LIKE '%Glam rock%' 
-GROUP BY 
-    band_name 
-ORDER BY 
-    lifespan DESC;
-
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
